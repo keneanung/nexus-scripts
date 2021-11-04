41e5bbd24e939acb851e8104e518f58fb736ea94
keneanung <keneanung@googlemail.com>
2021-11-04 21:41:02 +0100

    Initial setup

--- packages/nexus-event-bus/config.js
+++ packages/nexus-event-bus/config.js
@@ -1,12 +1,12 @@
 // Set up your project here
 const author = 'keneanung';  // author name on GitHub, might also be an org
-const projectName = 'iron-realms-nexus-script-template'; // project name on GitHub
-const libraryName = 'Greeter'; // name of the resulting library. Used by webpack to create a global name
-const npmPackageName = 'nexus_script_template'; // Name of the NPM package that should be published
-const projectDescription = 'Template for nexus scripts using typescript'; // Project description in the NPM package
+const projectName = 'nexus-event-bus'; // project name on GitHub
+const libraryName = 'EventBus'; // name of the resulting library. Used by webpack to create a global name
+const npmPackageName = 'nexus-event-bus'; // Name of the NPM package that should be published
+const projectDescription = 'Event bus/broker for the IRE Nexus client'; // Project description in the NPM package
 const authorEmail = 'keneanung@gmail.com' // author email address
-const docuTitle = 'Iron Realms Nexus Script Template'; // Title of the documentation website
-const docuTagline = 'Making Nexus scripts easier'; // Used for documentation website
+const docuTitle = 'Iron Realms Nexus Event Bus'; // Title of the documentation website
+const docuTagline = 'Event bus/broker for the IRE Nexus client'; // Used for documentation website
 
 
 // don't modify the exports below (except to add new fields)
