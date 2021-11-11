a8220bf9f7f50049581c8b639cb98325c4e93526
keneanung <keneanung@googlemail.com>
2021-11-11 15:22:49 +0100

    Use lower case variable name for webpack export

--- packages/nexus-event-bus/config.js
+++ packages/nexus-event-bus/config.js
@@ -1,7 +1,7 @@
 // Set up your project here
 const author = 'keneanung';  // author name on GitHub, might also be an org
 const projectName = 'nexus-event-bus'; // project name on GitHub
-const libraryName = 'EventBus'; // name of the resulting library. Used by webpack to create a global name
+const libraryName = 'eventBus'; // name of the resulting library. Used by webpack to create a global name
 const npmPackageName = 'nexus-event-bus'; // Name of the NPM package that should be published
 const projectDescription = 'Event bus/broker for the IRE Nexus client'; // Project description in the NPM package
 const authorEmail = 'keneanung@gmail.com' // author email address
