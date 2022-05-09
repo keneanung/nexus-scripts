95b914e6062210904dbc105e228dd279e897fd59
keneanung <keneanung@googlemail.com>
2022-05-09 23:07:54 +0200

    Add some definitions for package handling

--- packages/iron-realms-nexus-typings/definitions/client-api.d.ts
+++ packages/iron-realms-nexus-typings/definitions/client-api.d.ts
@@ -18,4 +18,30 @@ declare namespace client {
    * @param bgcolor Background color of the text
    */
   export function display_notice(text: string, fgcolor?: string, bgcolor?: string): void;
+
+  /**
+   * recursively adds the back reference of a parent group to each reflex.
+   */
+  export function reflexes_fix_parents(): void;
+
+  /**
+   * Array of reflex packages installed into the client.
+   */
+  const packages: Package[];
+
+  /**
+   * Removes a named package from the client.
+   *
+   * @param name Name of the package to remove from the client
+   */
+  export function package_remove(name: string): void;
+
+  /**
+   * Runs a Nexus function type reflex.
+   *
+   * @param functionName Name of the function to call
+   * @param args The argument to hand to the called function
+   * @param packageName The package to call the function in. Use the special value 'ALL' to call in all packages.
+   */
+  export function run_function(functionName: string, args: unknown | undefined, packageName: string): void;
 }
