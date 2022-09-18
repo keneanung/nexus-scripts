b42693673b401a3ca06515eb0d80df32445db410
keneanung <keneanung@googlemail.com>
2022-09-18 21:03:13 +0200

    Initial configuration

--- packages/nexus-package-manager/config.js
+++ packages/nexus-package-manager/config.js
@@ -1,13 +1,13 @@
 // Set up your project here
 const author = 'keneanung';  // author name on GitHub, might also be an org
-const projectName = 'iron-realms-nexus-script-template'; // project name on GitHub
-const libraryName = 'Greeter'; // name of the resulting library. Used by webpack to create a global name
+const projectName = 'nexus-package-manager'; // project name on GitHub
+const libraryName = 'PackageManager'; // name of the resulting library. Used by webpack to create a global name
 const npmAuthorName = 'keneanung'; // name of the author for NPM
-const npmPackageName = 'nexus_script_template'; // Name of the NPM package that should be published
-const projectDescription = 'Template for nexus scripts using typescript'; // Project description in the NPM package
+const npmPackageName = 'nexus-package-manager'; // Name of the NPM package that should be published
+const projectDescription = 'Package Manager for the Iron Realms Nexus 3 client'; // Project description in the NPM package
 const authorEmail = 'keneanung@gmail.com' // author email address
-const docuTitle = 'Iron Realms Nexus Script Template'; // Title of the documentation website
-const docuTagline = 'Making Nexus scripts easier'; // Used for documentation website
+const docuTitle = 'Iron Realms Nexus Package Manager'; // Title of the documentation website
+const docuTagline = 'The original package manager for Iron Realms Nexus'; // Used for documentation website
 
 
 // don't modify the exports below (except to add new fields)
