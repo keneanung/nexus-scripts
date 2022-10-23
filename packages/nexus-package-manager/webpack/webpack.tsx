b729299b6a488aae0f5755edc60f99abe84f267c
keneanung <keneanung@googlemail.com>
2022-10-23 20:52:53 +0200

    Don't add UI on unsupported platforms
    Currently, these platforms don't publish React at a global level and are
    missing a function for easy UI extensions.

--- packages/nexus-package-manager/webpack/webpack.tsx
+++ packages/nexus-package-manager/webpack/webpack.tsx
@@ -1,16 +1,20 @@
 // Webpack entrypoint.
 // Use this to do any browser specific initialization and export the module as global object.
 
-import { PackageManager, PackageManagerUi } from "../src/index"
-console.log('loading Package Manager version [AIV]{version}[/AIV]...')
-const packageManager = new PackageManager()
+import { PackageManager, PackageManagerUi } from '../src/index';
+console.log('loading Package Manager version [AIV]{version}[/AIV]...');
+const packageManager = new PackageManager();
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 globalThis.packageManager = packageManager;
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-nexusclient.ui().layout().register_custom_tab('npk_ui', <PackageManagerUi packageManager={packageManager} />);
+if (globalThis.React && nexusclient.ui().layout().register_custom_tab) {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  nexusclient.ui().layout().register_custom_tab('npk_ui', <PackageManagerUi packageManager={packageManager} />);
+}
 
-console.log("Package Manager loaded");
+console.log('Package Manager loaded');
 
 export default PackageManager;
