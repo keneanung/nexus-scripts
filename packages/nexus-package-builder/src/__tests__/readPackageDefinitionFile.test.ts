a3e8764c408b79d6c6589c3b5b1d19110206ff4b
keneanung <keneanung@gmail.com>
2023-06-07 00:38:29 +0200

    Fix package builder tests

--- packages/nexus-package-builder/src/__tests__/readPackageDefinitionFile.test.ts
+++ packages/nexus-package-builder/src/__tests__/readPackageDefinitionFile.test.ts
@@ -4,14 +4,15 @@ import * as fs from 'fs';
 import * as yaml from 'js-yaml';
 
 jest.mock('fs');
+jest.mock('js-yaml')
 const mockedFs = mocked(fs);
-let mockedYamlLoad: jest.SpyInstance<unknown, [str: string, opts?: yaml.LoadOptions]>;
+const mockedYaml = mocked(yaml);
 
 beforeEach(() => {
   mockedFs.readFileSync.mockReset();
   mockedFs.readFileSync.mockReturnValue('readFileSyncReturnValue');
-  mockedYamlLoad = jest.spyOn(yaml, 'load');
-  mockedYamlLoad.mockReturnValue({});
+  mockedYaml.load.mockReset();
+  mockedYaml.load.mockReturnValue({});
 });
 
 test('Should read the given file', () => {
@@ -26,25 +27,11 @@ test('Should read the file as UTF-8 to force the return of a string (compatibili
   expect(mockedFs.readFileSync).toHaveBeenCalledWith(expect.anything(), 'utf-8');
 });
 
-test('Should return the parsed string', () => {
-  mockedFs.readFileSync.mockReset();
-  mockedFs.readFileSync.mockReturnValue('type: group\ndescription: foo\nname: my package');
-  mockedYamlLoad.mockRestore();
-
-  const result = readPackageDefinitionFile('/absolute/file/path');
-
-  expect(result).toStrictEqual({
-    name: 'my package',
-    description: 'foo',
-    type: 'group',
-  });
-});
-
 test('Should hand the string read from the file to js-yaml', () => {
   mockedFs.readFileSync.mockReset();
   mockedFs.readFileSync.mockReturnValue('value read from file');
 
   readPackageDefinitionFile('/absolute/file/path');
 
-  expect(mockedYamlLoad).toHaveBeenCalledWith('value read from file');
+  expect(mockedYaml.load).toHaveBeenCalledWith('value read from file');
 });
