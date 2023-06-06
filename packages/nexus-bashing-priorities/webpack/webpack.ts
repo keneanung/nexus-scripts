7093f09dc4d87c6d3599d0f49888a8f08239b556
keneanung <keneanung@gmail.com>
2023-06-06 23:46:27 +0200

    Rename Bashing-js-2 module

--- packages/nexus-bashing-priorities/webpack/webpack.ts
+++ packages/nexus-bashing-priorities/webpack/webpack.ts
@@ -1,6 +1,6 @@
 // Webpack entrypoint.
 // Use this to do any browser specific initialization and export the module as global object.
 
-import { Bashing } from "../src/index"
+import { Bashing } from "./index"
 
 export default Bashing;
