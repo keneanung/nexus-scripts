0e526e94bb8cd9bcfe1fb3712bcb8b0761ab7662
keneanung <keneanung@gmail.com>
2023-06-14 09:54:59 +0000

    Remove now unnneeded magic comments

--- packages/nexus-queue-manager/src/nexusApi.ts
+++ packages/nexus-queue-manager/src/nexusApi.ts
@@ -1,6 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-
+/**
+ * Wrapper function for the send_commands function of the nexus client. This makes testing possible.
+ * @param {string} command The command to send.
+ */
 export const sendCommand = (command: string) => {
   nexusclient.send_commands(command, false);
 };
