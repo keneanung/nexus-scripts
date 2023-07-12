50a872c1a2ec3bb960eb4cf95c376bb81bc8dfa0
keneanung <keneanung@gmail.com>
2023-07-12 14:20:28 +0000

    refactor(nexus-simplified-scripting-converter): :technologist: move action templates to actual templates files and use jsrender as templating engine

--- packages/nexus-simplified-scripting-converter/src/index.ts
+++ packages/nexus-simplified-scripting-converter/src/index.ts
@@ -1,121 +1,30 @@
+import scriptTemplate from './templates/script.jsr';
+import functionTemplate from './templates/function.jsr';
+import disableTemplate from './templates/disable.jsr';
+import enableTemplate from './templates/enable.jsr';
+import commandTemplate from './templates/command.jsr';
+import notifyTemplate from './templates/notify.jsr'
+import jsrender from 'jsrender';
+
+const templates = jsrender.templates({
+  script: scriptTemplate,
+  function: functionTemplate,
+  disable: disableTemplate,
+  enable: enableTemplate,
+  command: commandTemplate,
+  notify: notifyTemplate,
+});
+
 const convertActions = (actions: client.Action[]) => {
-  let result = '';
+  const result = [];
   let index = 0;
 
   for (const action of actions) {
-    result += `// ${action.action} action (index ${index++})\n`
-    if (action.action === 'script') {
-      result += `{
-  ${action.script}
-}
-`;
-    } else if (action.action === 'function') {
-      if (action.fn && action.fn.length > 0) {
-        result += `nexusclient.reflexes().run_function("${action.fn}")\n`;
-      }
-    } else if (action.action === 'disable') {
-      result += `{
-  const reflex = nexusclient.reflexes().find_by_name("${action.type}", "${action.name}", true, false, current_package);
-  if(!reflex){
-    nexusclient.display_notice('Disable reflex: unable to find any ${action.type} called "${action.name}" in ' + (current_package?current_package:'global reflexes'), 'red');
-  }else{
-    nexusclient.reflexes().disable_reflex(reflex);
-  }
-}
-`;
-    } else if (action.action === 'enable') {
-      result += `{
-  const reflex = nexusclient.reflexes().find_by_name("${action.type}", "${action.name}", true, false, current_package);
-  if(!reflex){
-    nexusclient.display_notice('Enable reflex: unable to find any ${action.type} called "${action.name}" in ' + (current_package?current_package:'global reflexes'), 'red');
-  }else{
-    nexusclient.reflexes().enable_reflex(reflex);
-  }
-}
-`;
-    } else if (action.action === 'command') {
-      /*
-      args is the full match object for regex matches:
-      {
-      0: "test test",
-      1: " test",
-      groups: {foo: ' test'},
-      index: 10,
-      input: "this is a test test"
-      }
-      for begin of line and substring matches, it is null
-*/
-      result += `{
-    let cmd = "${action.command}";
-    if(args){
-      const prefix = args.input.substr(0, args.index);
-      const posend = args.index + args[0].length;
-      const suffix = args.input.substr(posend);
-      const replace = {};
-      replace["match"] = args[0];
-      replace["line"] = args.input;
-      replace["prefix"] = prefix;
-      replace["suffix"] = suffix;
-      if(args.length > 1){
-        for(let i = 1; i++; i < args.length){
-          replace[i] = args[i];
-        }
-      }
-      if(args.groups){
-        for(const group in args.groups){
-          replace[group] = args.groups[group];
-        }
-      }
-      cmd = nexusclient.variables().expand(cmd, replace);
-
-      ${
-        action.prefix_suffix
-          ? `if(prefix){
-        cmd = prefix + cmd;
-      }
-      if(suffix){
-        cmd = cmd + suffix;
-      }
-`
-          : ''
-      }
-    }
-    nexusclient.send_commands(cmd)
-  }
-  `;
-    } else if (action.action === 'notify') {
-      result += `{
-    let cmd = "${action.notice}";
-    if(args){
-      const prefix = args.input.substr(0, args.index);
-      const posend = args.index + args[0].length;
-      const suffix = args.input.substr(posend);
-      const replace = {};
-      replace["match"] = args[0];
-      replace["line"] = args.input;
-      replace["prefix"] = prefix;
-      replace["suffix"] = suffix;
-      if(args.length > 1){
-        for(let i = 1; i++; i < args.length){
-          replace[i] = args[i];
-        }
-      }
-      if(args.groups){
-        for(const group in args.groups){
-          replace[group] = args.groups[group];
-        }
-      }
-      cmd = nexusclient.variables().expand(cmd, replace);
-      cmd = nexusclient.variables().expand(cmd);
-      
-    }
-    nexusclient.display_notice(cmd, "${action.notice_fg}", "${action.notice_bg}")
-  }
-  `;
-    }
+    result.push(`// ${action.action} action (index ${index++})`);
+    result.push(templates.templates[action.action](action));
   }
 
-  return result;
+  return result.join('\n');
 };
 
 const convertReflex = (reflex: client.Reflex) => {
