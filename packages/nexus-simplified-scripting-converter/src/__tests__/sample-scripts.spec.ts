c32bc8d55a3d99f58fac5ce43f5ecff18b4e7157
keneanung <keneanung@gmail.com>
2023-08-10 07:44:16 +0000

    feat(nexus-simplified-scripting-converter): :white_check_mark: add infrastructure to test real world examples

---
+++ packages/nexus-simplified-scripting-converter/src/__tests__/sample-scripts.spec.ts
@@ -0,0 +1,10 @@
+import { Package } from '@keneanung/iron-realms-nexus-typings';
+import { convertPackage } from '..';
+import numpadMovement from './examples/numpad_movement.json'
+
+test('Numpad movement', () => {
+  const pkg = numpadMovement as Package;
+  convertPackage(pkg);
+
+  expect(pkg).toMatchSnapshot();
+})
